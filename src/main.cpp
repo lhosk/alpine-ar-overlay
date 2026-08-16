d6f4a72962e780bcd6ced7130a53c55ed22c21c6
lhosk <lhoskin.work@gmail.com>
2026-08-16 04:58:08 -0400

    link opencv, verify build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <opencv2/core.hpp>
 
 int main() {
     std::cout << "alpine-ar-overlay: build OK\n";
+    std::cout << "opencv version: " << CV_VERSION << "\n";
+
+    cv::Mat m = cv::Mat::eye(3, 3, CV_64F);
+    std::cout << "identity K:\n" << m << "\n";
     return 0;
 }
